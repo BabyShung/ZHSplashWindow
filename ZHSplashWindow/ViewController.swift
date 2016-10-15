df4c85cc105e7b4c5bad56137e8a9528a84b9321
Hao Zheng <babyshung8@gmail.com>
2016-10-14 21:32:36 -0400

    Initial Commit

---
+++ ZHSplashWindow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHSplashWindow
+//
+//  Created by Hao Zheng on 10/14/16.
+//  Copyright © 2016 Planhola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
